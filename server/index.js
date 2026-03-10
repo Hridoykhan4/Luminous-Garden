2646a3040d2ac089393c5994da0bca34d0ccdd28
Md. Toyob Uddin Hridoy <hridoykhan148385@gmail.com>
2026-03-11 01:15:02 +0600

    firebase initiated & morgan tested for server

--- server/index.js
+++ server/index.js
@@ -3,18 +3,41 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const morgan = require('morgan');
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 const app = express();
 
-// Middlewares
-app.use(cors())
-app.use(express.json())
-app.use((err, req, res, next) =>{
-    console.log(err);
-    res.status(500).send({message: 'Internal Server Error'})
-})
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
+const corsOptions = {
+  origin: ["http://localhost:5000"],
+  credentials: true,
+};
+
+// Middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(morgan('dev'))
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ message: "Internal Server Error" });
+});
+
+// Custom Middlewares
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) return res.status(403).send({ message: "Unauthorized Access" });
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.status(401).send({ message: "Unauthorized Access" });
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@happiness-hill.1es2bek.mongodb.net/?appName=Happiness-Hill`;
 
@@ -28,10 +51,31 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-  
-    console.log(
-      "Pinged deployment. Successfully connected to MongoDB!",
-    );
+    /* JWT related APIs Start */
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "300d",
+      });
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+
+    app.get("/logout", async (req, res) => {
+      try {
+        res
+          .clearCookie("token", {
+              ...cookieOptions,
+            maxAge: 0,
+          })
+          .send({ success: "Logged out" });
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    /* JWT related APIs End */
+
+    console.log("Pinged deployment. Successfully connected to MongoDB!");
   } finally {
     // await client.close();
   }
