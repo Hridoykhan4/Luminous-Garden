a8c2de1a26314281f6c193000eb214104d51cd6c
Md. Toyob Uddin Hridoy <hridoykhan148385@gmail.com>
2026-03-16 05:31:59 +0600

    initiated auth provider with context API

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import Router from './routers/Router'
+import AuthProvider from './providers/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <RouterProvider router={Router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
